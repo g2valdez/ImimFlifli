c3e916741d0a98546b5084e70bb9689798e1ed63
Guillermo Valdez <willie@Guillermos-MacBook-Pro.local>
2019-07-22 19:56:46 -0700

    addressed issue #2: save using original fidelity

--- imim_flifli.py
+++ imim_flifli.py
@@ -10,6 +10,20 @@ def format_ofile_name(fname, ver):
 	fname += '.png' 
 	return fname
 
+def make_left_image(image, reflect_width):
+	np_im = numpy.array(image)
+	left_reflection = np_im[:, :reflect_width, :]
+	left_reversed = numpy.fliplr(numpy.copy(left_reflection))
+	left_final = numpy.concatenate((left_reflection, left_reversed), axis=1)
+	return Image.fromarray(left_final)
+
+def make_right_image(image, reflect_width):
+	np_im = numpy.array(image)
+	right_reflection = np_im[:, reflect_width:, :]
+	right_reversed = numpy.fliplr(numpy.copy(right_reflection))
+	right_final = numpy.concatenate((right_reversed, right_reflection), axis=1)
+	return Image.fromarray(right_final)
+
 window = sg.Window('ImIm FliFli'). Layout(
 	[
 		[sg.Text('Filename')], [sg.InputText('B:/image0.jpg'), sg.FileBrowse()], 
@@ -21,6 +35,7 @@ window = sg.Window('ImIm FliFli'). Layout(
 	])
 
 reflect_line = None
+orig_image = None
 image = None
 mod1_image = None
 mod2_image = None
@@ -39,11 +54,11 @@ while True:
 	mod2_canvas = window.FindElement('mod2').TKCanvas
 
 	if event == 'OK':
-		image = Image.open(output[0])
-		ratio = image_height / image.height
+		orig_image = Image.open(output[0])
+		ratio = image_height / orig_image.height
 
 		# resize to max height 200
-		image = image.resize((int(image.width * ratio), image_height))
+		image = orig_image.resize((int(orig_image.width * ratio), image_height))
 
 		imTk = ImageTk.PhotoImage(image)
 		orig_canvas.config(width=imTk.width(), height=imTk.height())
@@ -51,7 +66,7 @@ while True:
 		reflect_line = orig_canvas.create_line(imTk.width()/2, 0, imTk.width()/2, imTk.height(), fill='red')
 
 
-	elif event == 1:
+	elif event == 1 and image != None:
 		# change position of line on orig_canvas
 		new_x_pos = (imTk.width() * (output[1]/100))
 		orig_canvas.coords(reflect_line, new_x_pos, 0, new_x_pos, imTk.height())
@@ -62,28 +77,18 @@ while True:
 		mod2_canvas.config(width=2 * imTk.width() * ((100 - output[1])/100), height=imTk.height())
 
 		if image != None:
-			np_im = numpy.array(image)
-			left_reflection = np_im[:, :reflect_width, :]
-			left_reversed = numpy.fliplr(numpy.copy(left_reflection))
-			left_final = numpy.concatenate((left_reflection, left_reversed), axis=1)
-			mod1_image = Image.fromarray(left_final)
-			mod1_imTk = ImageTk.PhotoImage(mod1_image)
+			mod1_imTk = ImageTk.PhotoImage(make_left_image(image, reflect_width))
 			mod1_canvas.create_image((reflect_width, mod1_imTk.height()/2), image=mod1_imTk)
 
-
-			right_reflection = np_im[:, reflect_width:, :]
-			right_reversed = numpy.fliplr(numpy.copy(right_reflection))
-			right_final = numpy.concatenate((right_reversed, right_reflection), axis=1)
-			mod2_image = Image.fromarray(right_final)
-			mod2_imTk = ImageTk.PhotoImage(mod2_image)
+			mod2_imTk = ImageTk.PhotoImage(make_right_image(image, reflect_width))
 			mod2_canvas.create_image((imTk.width() * ((100 - output[1])/100), mod2_imTk.height()/2), image=mod2_imTk)
 
 
 	elif event == "Save ver1" and mod1_image != None:
 		fname = format_ofile_name(output[0], '1')
-		mod1_image.save(fname)
+		make_left_image(orig_image, int(orig_image.width * (output[1]/100))).save(fname)
 
 	elif event == "Save ver2" and mod2_image != None:
 		fname = format_ofile_name(output[0], '2')
-		mod2_image.save(fname)
+		make_right_image(orig_image, int(orig_image.width * (output[1]/100))).save(fname)
 
